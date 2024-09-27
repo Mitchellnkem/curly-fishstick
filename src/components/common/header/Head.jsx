c3d7c5601214229de3bdd165eee134736858651f
Mitchellnkem <Mitchellnkemgmailcomgit config --global user.email Mitchellnkemgmail.com>
2024-09-27 12:42:42 +0000

    subsequent

---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Head = () => {
+  return (
+	<div>
+	  <section>
+		<div className='head'>
+			<div className='logo'>
+				<h1>CURLY-FISHSTICK</h1>
+			</div>
+
+		</div>
+	  </section>
+	</div>
+  )
+}
+
+export default Head
