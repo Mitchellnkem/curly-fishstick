d60249dced7e1983a88559d759cc6135aa0d1ae8
kod-python <kelvinlik0245@gmail.com>
2024-10-07 21:44:34 +0000

    solved notshowing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-/* eslint-disable no-unused-vars */
+
+
 import "./App.css"
 import Header from "./components/common/header/Header"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import About from "./components/about/About"
 import CourseHome from "./components/allcourses/CourseHome"
 import Team from "./components/team/Team"
@@ -17,15 +18,16 @@ function App  ()  {
     <>
       <Router>
         <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/courses' component={CourseHome} />
-          <Route exact path='/team' component={Team} />
-          <Route exact path='/pricing' component={Pricing} />
-          <Route exact path='/journal' component={Blog} />
-          <Route exact path='/contact' component={Contact} /> 
-        </Switch>
+        
+        <Routes>
+          <Route exact path='/' component={<Home />} />
+          <Route  path='/about' component={<About />} />
+          <Route  path='/courses' component={<CourseHome />} />
+          <Route  path='/team' component={<Team />} />
+          <Route  path='/pricing' component={<Pricing />} />
+          <Route  path='/journal' component={<Blog />} />
+          <Route  path='/contact' component={<Contact />} /> 
+        </Routes>
         <Footer />
       </Router>
     </>
@@ -33,3 +35,38 @@ function App  ()  {
 }
 
 export default App
+
+// import "./App.css";
+// import Header from "./components/common/header/Header";
+// import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+// import About from "./components/about/About";
+// import CourseHome from "./components/allcourses/CourseHome";
+// import Team from "./components/team/Team";
+// import Pricing from "./components/pricing/Pricing";
+// import Blog from "./components/blog/Blog";
+// import Contact from "./components/contact/Contact";
+// import Footer from "./components/common/footer/Footer";
+// import Home from "./components/home/Home";
+
+// function App() {
+//   return (
+//     <>
+//       <Router>
+    
+//       <Routes>
+//           <Route exact path="/" component={<Home />} />
+//           <Route  path="/about" component={<About />} />
+//           <Route  path="/courses" component={<CourseHome />} />
+//           <Route  path="/team" component={<Team />} />
+//           <Route  path="/pricing" component={<Pricing />} />
+//           <Route  path="/journal" component={<Blog />} />
+//           <Route  path="/contact" component={<Contact />} />
+//       </Routes>
+         
+     
+//       </Router>
+//     </>
+//   );
+// }
+
+// export default App;
--- src/components/team/TeamCard.jsx
+++ src/components/team/TeamCard.jsx
@@ -1,30 +1,88 @@
-
 import React from 'react'
-import { team } from 'react-router-dom'
+
+const team = [
+  {
+    cover: 'image1.jpg', // Replace with actual image path
+    name: 'John Doe',
+    work: 'Developer',
+  },
+  {
+    cover: 'image2.jpg',
+    name: 'Jane Smith',
+    work: 'Designer',
+  },
+  // Add more team members as needed
+]
 
 const TeamCard = () => {
   return (
-	<>
-	  {team.map((val) => (
-		// eslint-disable-next-line react/jsx-key
-		<div className="items shadow">
-			<div className="img">
-				<img src={val.cover} alt="" />
-				<div className="overlay">
-					<i className='fab fa-facebook-f icon'></i>
-					<i className='fab fa-twitter'></i>
-					<i className='fab fa-instagram icon'></i>
-					<i className='fab fa-tiktok icon'></i>
-				</div>
-			</div>
-			<div className="details">
-				<h2>{val.name}</h2>
-				<p>{val.work}</p>
-			</div>
-		</div>
-	  ) )}
-	</>
+    <>
+      {team.map((val, index) => (
+        <div key={index} className="items shadow">
+          <div className="img">
+            <img src={val.cover} alt={val.name} />
+            <div className="overlay">
+              <i className='fab fa-facebook-f icon'></i>
+              <i className='fab fa-twitter'></i>
+              <i className='fab fa-instagram icon'></i>
+              <i className='fab fa-tiktok icon'></i>
+            </div>
+          </div>
+          <div className="details">
+            <h2>{val.name}</h2>
+            <p>{val.work}</p>
+          </div>
+        </div>
+      ))}
+    </>
   )
 }
 
 export default TeamCard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import { team } from 'react-router-dom'
+// // import team from 'react-router'
+
+// const TeamCard = () => {
+//   return (
+// 	<>
+// 	  {team.map((val) => (
+// 		// eslint-disable-next-line react/jsx-key
+// 		<div className="items shadow">
+// 			<div className="img">
+// 				<img src={val.cover} alt="" />
+// 				<div className="overlay">
+// 					<i className='fab fa-facebook-f icon'></i>
+// 					<i className='fab fa-twitter'></i>
+// 					<i className='fab fa-instagram icon'></i>
+// 					<i className='fab fa-tiktok icon'></i>
+// 				</div>
+// 			</div>
+// 			<div className="details">
+// 				<h2>{val.name}</h2>
+// 				<p>{val.work}</p>
+// 			</div>
+// 		</div>
+// 	  ) )}
+// 	</>
+//   )
+// }
+
+// export default TeamCard
